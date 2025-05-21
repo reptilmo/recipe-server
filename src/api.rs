89ed3aa58deb8ef289b22d2e20bdfde5feb31873
kostya <reptilmo@gmail.com>
2025-05-20 23:22:05 -0700

    Added handle parse recipe id error.

--- src/api.rs
+++ src/api.rs
@@ -6,13 +6,16 @@ pub async fn get_recipe_by_id(
     Path(recipe_id): Path<String>,
 ) -> Result<response::Response, http::StatusCode> {
     let appstate = state.read().await;
-    let id = recipe_id.parse::<i64>().unwrap(); // TODO:
-
-    match database::fetch_recipe(&appstate.db, id).await {
-        Ok(recipe) => Ok(recipe.into_response()),
-        Err(e) => {
-            log::error!("api failed to fetch recipe id={}, err={}", id, e);
-            Err(http::StatusCode::NOT_FOUND)
+    if let Ok(id) = recipe_id.parse::<i64>() {
+        match database::fetch_recipe(&appstate.db, id).await {
+            Ok(recipe) => Ok(recipe.into_response()),
+            Err(e) => {
+                log::error!("api failed to fetch recipe id={}, err={}", id, e);
+                Err(http::StatusCode::NOT_FOUND)
+            }
         }
+    } else {
+        log::error!("api failed to parse recipe id={}", recipe_id);
+        Err(http::StatusCode::BAD_REQUEST)
     }
 }
